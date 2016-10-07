d825db5d7117c6232c40ea3fb5717f0a9e015381
BrunoFelipi <bruno.souza@senior.com.br>
2016-10-07 00:13:42 -0300

    Ajustes
    - Arrumado padding left na tela de login

--- controllers/cadastrarPontoCtrl.js
+++ controllers/cadastrarPontoCtrl.js
@@ -1,5 +1,20 @@
 app.controller('cadastrarPontoCtrl', function($scope, $location){
 
+
+  $scope.initMap = function () {
+
+      var uluru = {lat: $scope.latitude, lng: $scope.longitude};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+      });
+
+      var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+      });
+    }
+
     $scope.limparCampos = function(){
         $location.path('cadastrarPonto');
     }
