d4ca565dd068545199ad6323c4bfc4aac5eedec8
BrunoFelipi <bruno.souza@senior.com.br>
2016-10-07 18:12:32 -0300

    Ajustes

--- controllers/cadastrarPontoCtrl.js
+++ controllers/cadastrarPontoCtrl.js
@@ -1,19 +1,7 @@
 app.controller('cadastrarPontoCtrl', function($scope, $location){
 
-
-  $scope.initMap = function () {
-
-      var uluru = {lat: $scope.latitude, lng: $scope.longitude};
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
-      });
-
-      var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-      });
-    }
+	$scope.latitude = '-26.9166207';
+	$scope.longitude = '-49.0717429';
 
     $scope.limparCampos = function(){
         $location.path('cadastrarPonto');
