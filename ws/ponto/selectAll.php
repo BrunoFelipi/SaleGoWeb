1b485b3e2e67db50d81d93041c2fffe393fb9926
BrunoFelipi <bruno.souza@senior.com.br>
2016-11-21 23:19:00 -0200

    little ajustes

--- ws/ponto/selectAll.php
+++ ws/ponto/selectAll.php
@@ -2,11 +2,12 @@
     include '../conexao.php';
     $data = json_decode(file_get_contents('php://input'), true);
 
-    $id = $data['id'];
+    $idEmpresa = $data['id'];
 
-    $sql = "select id, endereco, descricao, raioAlcance, valor, tipoDesconto, ativo FROM ponto WHERE id='$id'";
+    $sql = "select id, endereco, descricao, raioAlcance, valor, tipoDesconto, ativo FROM ponto WHERE id='$idEmpresa'";
 
     $rs = mysqli_query($conexao, $sql);
+
 
     if(mysqli_num_rows($rs) == 0){
         echo 'false';
