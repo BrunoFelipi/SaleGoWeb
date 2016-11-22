e51b8a96145a3f8cc97c0da5754c5c43265326df
BrunoFelipi <bruno.souza@senior.com.br>
2016-11-22 16:03:31 -0200

    Testes

--- controllers/homeCtrl.js
+++ controllers/homeCtrl.js
@@ -1,14 +1,14 @@
 app.controller('homeCtrl', function($scope, $rootScope, $location, PontoService){
-
+/*
     if($rootScope.empresaAtiva.id < 1){
         $location.path('login');
         return;
     }
-
+*/
     $scope.empresaAtiva = $rootScope.empresaAtiva;
     $scope.pontos = [];
 
-    var promise = PontoService.selectAll($scope.empresaAtiva.id);
+    var promise = PontoService.selectAll(1);
 
     promise.then(function(response){
         $scope.pontos = response.data;
--- sql/script.sql
+++ sql/script.sql
@@ -17,7 +17,21 @@ CREATE TABLE  ponto (
   raioAlcance varchar(100) DEFAULT NULL,
   valor varchar(100) DEFAULT NULL,
   tipoDesconto varchar(10) DEFAULT NULL,
+  qtdClientes int(8),
   ativo char(1)
+);
+
+insert into ponto values (0, 1, 'Rua São Paulo, 825', 'Senior Sistemas', '100', '10', '%', 3, 's');
+insert into ponto values (0, 1, 'Rua Nereu Ramos, 25', 'Centro', '100', '200', 'R$', 0, 's');
+insert into ponto values (0, 1, 'Rua Virginia Ferreira, 181', 'Casa', '130', '10', '%', 0, 'n');
+insert into ponto values (0, 1, 'Rua 7 de Setembro, 1065', 'Shopping Neu', '50', '10', '%', 10, 's');
+insert into ponto values (0, 1, 'Rua Timbó, 25', 'CREA', '300', '20', 'R$', 2, 's');
+
+create table pontosPegos(
+  id int (8) primary key auto_increment,
+  idPonto int(8),
+  idCliente int(8),
+  dia datetime
 );
 
 CREATE TABLE  empresa (
@@ -29,4 +43,12 @@ CREATE TABLE  empresa (
   ativo char(1) COLLATE utf8_unicode_ci DEFAULT NULL,
   logo int(11) DEFAULT NULL,
   KEY `emp_logo_fk` (`logo`)
+);
+
+CREATE TABLE  cliente (
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nome varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  cpf varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  email varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  senha varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
 );
