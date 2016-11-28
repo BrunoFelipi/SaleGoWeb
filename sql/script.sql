e5afc556afc581087e22dc71720a2937e2f8078a
BrunoFelipi <bruno.souza@senior.com.br>
2016-11-28 17:09:09 -0200

    Base de teste

--- sql/script.sql
+++ sql/script.sql
@@ -1,14 +1,3 @@
-create table usuario(
-  id int auto_increment primary key,
-  nome varchar(100),
-  email varchar(50),
-  senha varchar(50),
-  token varchar(32),
-  tipoPessoa char(1),
-  identificacao varchar(20),
-  ativo char(1)
-);
-
 CREATE TABLE  ponto (
   id int(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   idEmpresa int(8) DEFAULT NULL,
@@ -40,6 +29,9 @@ create table pontosPegos(
   ativo char(1)
 );
 
+insert into pontospegos values (0,12,3,1,NOW(),'s');
+insert into pontospegos values (0,13,3,2,NOW(),'s');
+
 CREATE TABLE  empresa (
   id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   nome varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -52,8 +44,8 @@ CREATE TABLE  empresa (
   KEY `emp_logo_fk` (`logo`)
 );
 
-insert into empresa values (1,'teste','123456789','teste@teste.com.br','10b9231267e5844f485572e2c3ae14b1','698dc19d489c4e4db73e28a713eab07b','s',0);
-
+insert into empresa values (0, 'Empresa 1', '43.411.029/0001-79', 'empresa1@empresa1.com.br', 'token', '698dc19d489c4e4db73e28a713eab07b', 's', 0);
+insert into empresa values (0, 'Empresa 2', '16.524.240/0001-25', 'empresa2@empresa2.com.br', 'token', '698dc19d489c4e4db73e28a713eab07b', 's', 0);
 
 CREATE TABLE  cliente (
   id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -62,3 +54,8 @@ CREATE TABLE  cliente (
   email varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   senha varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
 );
+
+insert into cliente values (0,'Cliente 1','094.603.274-27','cliente1@cliente1.com.br','698dc19d489c4e4db73e28a713eab07b');
+insert into cliente values (0,'Cliente 2','252.783.749-26','cliente1@cliente2.com.br','698dc19d489c4e4db73e28a713eab07b');
+insert into cliente values (0,'Cliente 3','233.672.226-74','cliente1@cliente3.com.br','698dc19d489c4e4db73e28a713eab07b');
+insert into cliente values (0,'Cliente 4','348.158.073-88','cliente1@cliente4.com.br','698dc19d489c4e4db73e28a713eab07b');
