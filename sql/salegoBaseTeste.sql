93ab39357d80f3ab9a250751bea017b935bfcfe2
BrunoFelipi <bruno.souza@senior.com.br>
2016-11-29 13:28:32 -0200

    Base Teste

---
+++ sql/salegoBaseTeste.sql
@@ -0,0 +1,19 @@
+-- Popular tabela Cliente
+insert into cliente values (0,'Cliente 1','094.603.274-27','cliente1@cliente1.com.br','698dc19d489c4e4db73e28a713eab07b');
+insert into cliente values (0,'Cliente 2','252.783.749-26','cliente1@cliente2.com.br','698dc19d489c4e4db73e28a713eab07b');
+insert into cliente values (0,'Cliente 3','233.672.226-74','cliente1@cliente3.com.br','698dc19d489c4e4db73e28a713eab07b');
+insert into cliente values (0,'Cliente 4','348.158.073-88','cliente1@cliente4.com.br','698dc19d489c4e4db73e28a713eab07b');
+insert into cliente values (0,'Cliente 5','172.117.778-77','cliente1@cliente5.com.br','698dc19d489c4e4db73e28a713eab07b');
+
+-- Popular tabela Empresa
+insert into empresa values (0,'Empresa 1','43.411.029/0001-79','empresa1@empresa1.com.br','token','698dc19d489c4e4db73e28a713eab07b','s',0);
+insert into empresa values (0,'Empresa 2','16.524.240/0001-25','empresa2@empresa2.com.br','token','698dc19d489c4e4db73e28a713eab07b','s',0);
+
+-- Popular tabela Pontos
+** Cadastrar pelo sistema
+
+-- Popular tabela PontosPegos
+insert into pontospegos values (0,12,3,1,NOW());
+insert into pontospegos values (0,12,3,2,NOW());
+insert into pontospegos values (0,12,3,1,NOW());
+insert into pontospegos values (0,12,3,1,NOW());
