9ea8dc2733ef0f98819a57b9a8eb2e8415151edf
BrunoFelipi <bruno.souza@senior.com.br>
2016-11-29 13:26:53 -0200

    Iniciado ajuste da modal da página relatório

--- controllers/clienteCtrl.js
+++ controllers/clienteCtrl.js
@@ -7,6 +7,9 @@ app.controller('clienteCtrl', function($scope, $rootScope, $location, RelatorioS
     $scope.valorTotalPorcentagem = 0;
     $scope.valorTotalReais = 0;
 
+    var totalPorcentagem = 0;
+    var totalReais = 0;
+
     var promise = RelatorioService.carregarClienteSelecionado($routeParams.id);
     promise.then(function(response) {
         $scope.cliente = response.data;
@@ -17,51 +20,43 @@ app.controller('clienteCtrl', function($scope, $rootScope, $location, RelatorioS
     var promise = RelatorioService.carregarPontosCliente($routeParams.id);
     promise.then(function(response) {
         $scope.pontosCliente = response.data;
-
-        for (var i = 0; i < $scope.pontosCliente.length; i++) {
-            console.log($scope.pontosCliente[i]);
-            if ($scope.pontosCliente[i].tipoDesconto == 'R$') {
-                $scope.valorTotalReais = $scope.valorTotalReais + $scope.pontosCliente[i].qtdPontos;
-            } else {
-                $scope.valorTotalPorcentagem = $scope.valorTotalPorcentagem + $scope.pontosCliente[i].valor;
-            }
-        }
-
     }, function(error) {
         Materialize.toast('Erro de conexão com o banco', 4000);
     });
 
     $scope.existInCarrinho = function(id) {
-        if ($scope.carrinhoPontos.indexOf(id) !== -1) {
-            return true;
-        } else {
-            return false;
+        for (var i = 0; i < $scope.carrinhoPontos.length; i++) {            
+            if ($scope.carrinhoPontos[i].id == id) {
+                return true;
+            }
         }
+        return false;
     }
 
     $scope.addCarrinhoPontos = function(idPonto) {
 
         var promise = PontoService.select(idPonto);
         promise.then(function(response) {
-            console.log(response.data);
-            $scope.carrinhoPontos.push(response.data);
+            $scope.carrinhoPontos.push(response.data[0]);            
+        }, function(error) {
+            Materialize.toast('Erro de conexão com o banco', 4000);
+        });
+    }
+
+    $scope.remCarrinhoPontos = function(idPonto) {
+
+        var promise = PontoService.select(idPonto);
+        promise.then(function(response) {
+            var index = $scope.carrinhoPontos.indexOf(idPonto);
+            $scope.carrinhoPontos.splice(index, 1);
         }, function(error) {
             Materialize.toast('Erro de conexão com o banco', 4000);
         });
 
-        $('.tooltipped').tooltip('remove');
     }
 
-    $scope.remCarrinhoPontos = function(idPonto) {
-        var index = $scope.carrinhoPontos.indexOf(idPonto);
-        $scope.carrinhoPontos.splice(index, 1);
-        $('.tooltipped').tooltip('remove');
-    }
-
-    $scope.descontarPontos = function(idCliente) {
-
-
-
+    $scope.descontarPontos = function() {
+        console.log($scope.cliente[0].id);
     }
 
 });
