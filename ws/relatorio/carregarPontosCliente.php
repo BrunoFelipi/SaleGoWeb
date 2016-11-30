e371d27755ececc8b5144b0b0a5e957676417fc6
BrunoFelipi <bruno.souza@senior.com.br>
2016-11-30 00:00:34 -0200

    Ajustado para apagar os pontos do cliente quando os mesmos estiverem vencidos.

--- ws/relatorio/carregarPontosCliente.php
+++ ws/relatorio/carregarPontosCliente.php
@@ -4,7 +4,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 $idCliente = $data['idCliente'];
 
-$sql = "SELECT P.id, P.endereco, P.latitude, P.longitude, P.descricao, P.raioAlcance, P.valor, P.tipoDesconto, P.dataVencimento, P.ativo, PP.id, PP.idPonto, PP.idEmpresa, PP.idCliente, PP.dia, PP.ativo  FROM pontospegos PP, ponto P where PP.idCliente='$idCliente' AND PP.idPonto = P.id AND PP.ativo='s'";
+$sql = "SELECT P.id, P.endereco, P.latitude, P.longitude, P.descricao, P.raioAlcance, P.valor, P.tipoDesconto, P.dataVencimento, P.ativo, PP.id, PP.idPonto, PP.idEmpresa, PP.idCliente, PP.dia, PP.ativo  FROM pontospegos PP, ponto P where PP.idCliente='$idCliente' AND PP.idPonto = P.id AND PP.ativo='s' AND P.dataVencimento > NOW()";
 
 $rs = mysqli_query($conexao, $sql);
 
