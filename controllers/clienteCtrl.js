bd2ff090e535cb593bc28f0f5bce1ef338b425a5
BrunoFelipi <bruno.souza@senior.com.br>
2016-11-29 23:48:11 -0200

    Fazendo alterações na tela de descontar pontos;

--- controllers/clienteCtrl.js
+++ controllers/clienteCtrl.js
@@ -1,31 +1,29 @@
-app.controller('clienteCtrl', function($scope, $rootScope, $location, RelatorioService, PontoService, $routeParams, $filter) {
+app.controller('clienteCtrl', function ($scope, $rootScope, $location, RelatorioService, PontoService, $routeParams, $filter) {
 
     $scope.cliente = [];
     $scope.pontosCliente = [];
     $scope.carrinhoPontos = [];
 
-    $scope.valorTotalPorcentagem = 0;
-    $scope.valorTotalReais = 0;
+    $scope.valorTotalPontos = 0;
 
-    var totalPorcentagem = 0;
-    var totalReais = 0;
+    var valorTotalPontosC = 0;
 
     var promise = RelatorioService.carregarClienteSelecionado($routeParams.id);
-    promise.then(function(response) {
+    promise.then(function (response) {
         $scope.cliente = response.data;
-    }, function(error) {
+    }, function (error) {
         Materialize.toast('Erro de conexão com o banco', 4000);
     });
 
     var promise = RelatorioService.carregarPontosCliente($routeParams.id);
-    promise.then(function(response) {
+    promise.then(function (response) {
         $scope.pontosCliente = response.data;
-    }, function(error) {
+    }, function (error) {
         Materialize.toast('Erro de conexão com o banco', 4000);
     });
 
-    $scope.existInCarrinho = function(id) {
-        for (var i = 0; i < $scope.carrinhoPontos.length; i++) {            
+    $scope.existInCarrinho = function (id) {
+        for (var i = 0; i < $scope.carrinhoPontos.length; i++) {
             if ($scope.carrinhoPontos[i].id == id) {
                 return true;
             }
@@ -33,30 +31,59 @@ app.controller('clienteCtrl', function($scope, $rootScope, $location, RelatorioS
         return false;
     }
 
-    $scope.addCarrinhoPontos = function(idPonto) {
+    $scope.addCarrinhoPontos = function (idPonto) {
 
         var promise = PontoService.select(idPonto);
-        promise.then(function(response) {
-            $scope.carrinhoPontos.push(response.data[0]);            
-        }, function(error) {
+        promise.then(function (response) {
+            $scope.carrinhoPontos.push(response.data[0]);
+            
+            var valor = 0;
+            valorTotalPontosC = 0;
+            for (var i = 0; i < $scope.carrinhoPontos.length; i++) {
+                
+                valor = $scope.carrinhoPontos[i].valor;
+                valorTotalPontosC = valorTotalPontosC + parseFloat(valor);
+                $scope.valorTotalPontos = valorTotalPontosC;
+            }
+
+        }, function (error) {
             Materialize.toast('Erro de conexão com o banco', 4000);
         });
+
     }
 
-    $scope.remCarrinhoPontos = function(idPonto) {
+    $scope.remCarrinhoPontos = function (idPonto) {
 
         var promise = PontoService.select(idPonto);
-        promise.then(function(response) {
+        promise.then(function (response) {
             var index = $scope.carrinhoPontos.indexOf(idPonto);
-            $scope.carrinhoPontos.splice(index, 1);
-        }, function(error) {
+
+            for (var i = 0; i < $scope.carrinhoPontos.length; i++) {
+                if ($scope.carrinhoPontos[i].id == idPonto) {
+                    var valor = $scope.carrinhoPontos[i].valor;
+                    valorTotalPontosC -= parseFloat(valor);
+                    $scope.valorTotalPontos = valorTotalPontosC;
+                    $scope.carrinhoPontos.splice(index, 1);
+                }
+            }
+            
+        }, function (error) {
             Materialize.toast('Erro de conexão com o banco', 4000);
         });
 
     }
 
-    $scope.descontarPontos = function() {
+    $scope.openModal = function () {
+
+        console.log('aaa');
+        $scope.valorTotalPontos = valorTotalPontosC;
+
+    }
+
+    $scope.descontarPontos = function () {
         console.log($scope.cliente[0].id);
+
+
     }
 
 });
